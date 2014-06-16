39285832a53a3571e9c07423277bdd73612f7406
PradatiusD <danielprada2012@gmail.com>
2014-06-16 09:31:06 -0400

    made it so clicking on home scrolls to top

--- nav-active.js
+++ nav-active.js
@@ -3,9 +3,18 @@
 	var $menu = $('#menu-main-menu');
 	var $menuLi = $menu.find('li');
 
-	$menu.find('a').click(function(){
+	$menu.find('a').click(function(e){
+
+		var $this = $(this);
+
 		$menuLi.removeClass('active');
-		$(this).parent().addClass('active');
+		$this.parent().addClass('active');
+
+		// Fix first link and make it scroll to top
+		if ($this.parent().index() === 0) {
+			e.preventDefault();
+			$("html, body").animate({ scrollTop: 0 }, "slow");
+		}
 	});
 
 	var selectorStrings = ['.register','.eventschedule','.speakers-home','.visitors','#contact'];
@@ -21,5 +30,5 @@
 	for (var i = 0; i < selectorStrings.length; i++) {
 		activeWhenInView(i);
 	}
-	
+
 })(jQuery);
