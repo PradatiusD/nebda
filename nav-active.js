5ed3d242be514a76e389cac627149248faad89c8
PradatiusD <danielprada2012@gmail.com>
2014-06-16 09:22:54 -0400

    Correctly fixed script to use jQuery waypoints

--- nav-active.js
+++ nav-active.js
@@ -1,25 +1,5 @@
 (function AddActiveClassToMenu($){
 
-	/* jQuery plugin to check if on Screen */
-
-	$.fn.isOnScreen = function(){
-
-		var win = $(window);
-
-		var viewport = {
-			top : win.scrollTop(),
-			left : win.scrollLeft()
-		};
-		viewport.right = viewport.left + win.width();
-		viewport.bottom = viewport.top + win.height();
-
-		var bounds = this.offset();
-		bounds.right = bounds.left + this.outerWidth();
-		bounds.bottom = bounds.top + this.outerHeight();
-
-		return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
-	};
-
 	var $menu = $('#menu-main-menu');
 	var $menuLi = $menu.find('li');
 
@@ -30,20 +10,16 @@
 
 	var selectorStrings = ['.register','.eventschedule','.speakers-home','.visitors','#contact'];
 
-	for (var i = 0; i < selectorStrings.length; i++) {
-		$(selectorStrings[i]).addClass('pd-section');
+	function activeWhenInView(iterator) {
+		$(selectorStrings[iterator]).waypoint(function(){
+			$menuLi
+				.removeClass('active')
+				.eq(iterator+1).addClass('active');
+		});
 	}
 
-	$sections = $('.pd-section');
-
-	$(window).scroll(function(){
-		$sections.each(function(index){
-			if ($(this).isOnScreen()) {
-				$menuLi
-					.removeClass('active')
-					.eq(index+1).addClass('active');
-			}
-		});
-	});
-
+	for (var i = 0; i < selectorStrings.length; i++) {
+		activeWhenInView(i);
+	}
+	
 })(jQuery);
